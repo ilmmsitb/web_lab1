35447249a7a5bf9e8b931a7ee6d96f03192a2a7a
ilmmsitb <xolodanyuta@yandex.ru>
2025-10-15 22:26:00 +0200

    Update process.php

--- nginx-lab/www/process.php
+++ nginx-lab/www/process.php
@@ -1,15 +1,15 @@
 <?php
 session_start();
 
-// Получаем данные формы
+// Получаем данные формы через $_POST
 $name = htmlspecialchars($_POST['name']);
-$excursion_date = htmlspecialchars($_POST['excursion_date']);
-$route = htmlspecialchars($_POST['route']);
+$excursion_date = htmlspecialchars($_POST['excursion_date'] ?? '');
+$route = htmlspecialchars($_POST['route'] ?? '');
 $audio_guide = isset($_POST['audio_guide']) ? 'Да' : 'Нет';
-$language = htmlspecialchars($_POST['language']);
-$email = htmlspecialchars($_POST['email']);
+$language = htmlspecialchars($_POST['language'] ?? '');
+$email = htmlspecialchars($_POST['email'] ?? '');
 
-// Сохраняем данные в сессию
+// Сохраняем все данные в сессию
 $_SESSION['name'] = $name;
 $_SESSION['excursion_date'] = $excursion_date;
 $_SESSION['route'] = $route;
@@ -17,11 +17,7 @@ $_SESSION['audio_guide'] = $audio_guide;
 $_SESSION['language'] = $language;
 $_SESSION['email'] = $email;
 
-// Сохраняем данные в файл data.txt
-$line = $name . ";" . $email . "\n";
-file_put_contents("data.txt", $line, FILE_APPEND);
-
-// Перенаправляем обратно на главную страницу
+// После сохранения в сессию перенаправляем обратно на главную страницу
 header("Location: index.php");
 exit();
 ?>
