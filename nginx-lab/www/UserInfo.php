66ff73f77dfe5db57b530bd55975499cec4d2756
Pavel <xolodanyuta@yandex.ru>
2025-10-23 20:31:47 +0200

    Create UserInfo.php

---
+++ nginx-lab/www/UserInfo.php
@@ -0,0 +1,11 @@
+<?php
+// UserInfo.php
+class UserInfo {
+    public static function getInfo(): array {
+        return [
+            'ip' => $_SERVER['REMOTE_ADDR'] ?? '',
+            'user_agent' => $_SERVER['HTTP_USER_AGENT'] ?? '',
+            'time' => date('Y-m-d H:i:s')
+        ];
+    }
+}
