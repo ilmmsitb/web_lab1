32360cf639e44e42e783a37f4734239f2e0f1115
ilmmsitb <xolodanyuta@yandex.ru>
2025-11-13 22:37:48 +0200

    Create ClientFactory.php

---
+++ nginx-lab/www/ClientFactory.php
@@ -0,0 +1,18 @@
+<?php
+namespace App;
+
+use GuzzleHttp\Client;
+
+class ClientFactory
+{
+    public static function make(string $baseUri): Client
+    {
+        return new Client([
+            'base_uri' => $baseUri,
+            'timeout'  => 10.0,
+            'headers' => [
+                'Content-Type' => 'application/json',
+            ]
+        ]);
+    }
+}
